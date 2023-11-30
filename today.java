1a936d2cf4717282625715050035e94f39b6bda0
Suraj-3579 <152250226+Suraj-3579@users.noreply.github.com>
2023-11-30 11:12:38 +0530

    Create today.java

---
+++ today.java
@@ -0,0 +1,12 @@
+class today
+  {
+    public static void main(String[] a)
+    {
+     Scanner sc=new Sacnner(System.in);
+      int i=sc.nextInt();
+      for(i=0;i<=10;i++)
+        {
+          System.out.println(i);
+        }
+    }
+  }

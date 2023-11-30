6eec30b31403d624b14bfc1226bca91ae1e281f1
Suraj-3579 <152250226+Suraj-3579@users.noreply.github.com>
2023-11-30 11:24:55 +0530

    Update today.java

--- today.java
+++ today.java
@@ -2,9 +2,8 @@ class today
   {
     public static void main(String[] a)
     {
-     Scanner sc=new Scanner(System.in);
-      int i=sc.nextInt();
-      for(i=0;i<=10;i++)
+     
+      for(int i=0;i<=10;i++)
         {
           System.out.println(i);
         }
